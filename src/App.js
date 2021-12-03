23ed95d761350b010a2f86141fd1eef2c2b6c3c3
Matt Wojcieszek <wojo1330@gmail.com>
2021-12-03 14:04:44 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+export default function App() {
+  let people = [
+    ["Ariana", "Grande"],
+    ["Bruno", "Mars"],
+    ["Kendrick", "Lamar"],
+    ["Doja", "Cat"]
+  ];
+
+  // TODO 1: print out bruno mars's name by referencing people
+  // TODO 2: change people to be an array of objects
+  // TODO 3: print out bruno mars's name by referencing people
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
